a52a5ba2d2dce6b72834f7b19c7377e067fda5b5
Nathan Glenn <garfieldnate@gmail.com>
2013-09-16 12:24:32 -0700

    Added TODO note about getting rid of timestamps

--- lib/WicsGui/Logger.pm
+++ lib/WicsGui/Logger.pm
@@ -6,6 +6,8 @@ use Wx;
 
 # Create logging methods: debug, info, etc.
 foreach my $method ( Log::Any->logging_methods() ) {
+    # TODO: would rather use WX::Log::GetActiveTarget()->DoLogString,
+    # but this causes errors.
     make_method($method, sub { Wx::LogMessage($_[1], undef) });
 }
 
