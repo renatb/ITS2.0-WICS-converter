daa111d4c8fa9c7d5c48eff8657e8df5a4b36a15
Nathan Glenn <garfieldnate@gmail.com>
2013-09-18 14:40:28 -0700

    in case the return value from get_xpath is undef, return an empty list

--- lib/ITS/DOM/Node.pm
+++ lib/ITS/DOM/Node.pm
@@ -155,7 +155,7 @@ sub get_xpath {
     }else{
         push @nodes, ITS::DOM::Value->new($object);
     }
-    return @nodes;
+    return @nodes || ();
 }
 
 #simple dictionary-lookup sub for parameter handling in get_xpath method
