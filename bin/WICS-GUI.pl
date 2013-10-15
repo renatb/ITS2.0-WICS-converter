71a630c9a6ec9ff5f77666aae891d6b10f10a49b
Nathan Glenn <garfieldnate@gmail.com>
2013-10-15 13:05:27 -0700

    Changed encoding(UTF-8) to utf8 after having trouble with 'invalid file mode' error

--- bin/WICS-GUI.pl
+++ bin/WICS-GUI.pl
@@ -372,7 +372,7 @@ sub _convert_files {
                 "\n----------\n$path\n----------\n");
             my $html = $transformer->($path);
             my $new_path = _get_new_path($path, $output_ext);
-            my $fh = $new_path->filehandle('>:encoding(UTF-8)');
+            my $fh = $new_path->filehandle('>:utf8');
             print $fh ${ $html };
             $text->SetDefaultStyle($done_style);
             $text->AppendText("\nwrote $new_path\n");
@@ -447,7 +447,6 @@ sub new {
           output_ext => 'xlf',
         },
     ];
-
 
     # metrics
     $self->{margin} = 5;
