f42c95bede5133f72a28e3a928d2ba55180f01f4
Nathan Glenn <garfieldnate@gmail.com>
2013-10-17 19:36:25 -0700

    Added a User guide

--- scripts/GenerateDocs.pl
+++ scripts/GenerateDocs.pl
@@ -10,18 +10,28 @@ my $proj = $cwd->parent;
 #lib and bin contain code with embedded POD
 my $lib = path($proj, 'lib');
 my $bin = path($proj, 'bin');
+my $docs = path($proj, 'docs');
 
 #output in docs/modules
-my $out = path($proj, 'docs', 'modules');
+my $out = path($docs, 'modules');
 # make sure output directory is clean
 if($out->is_dir){
     $out->remove_tree;
 }
 $out->mkpath;
 
-#copy the cpanStyle css into the output directory
-copy(path($Bin, 'cpanStyle.css'), path($out, 'cpanStyle.css'));
-
 my $batchconv = Pod::Simple::HTMLBatch->new;
 $batchconv->add_css( 'cpanStyle.css', 1, 'cpanStyle');
-$batchconv->batch_convert( ["$lib", "$bin"], $out );
+$batchconv->batch_convert( ["$lib", "$bin", $docs], $out );
+
+#copy the cpanStyle css and the WICS-GUI image into the output directory
+my @files = qw(
+    cpanStyle.css
+    WICS-GUI.png
+    WICS-GUI-choose-file.png
+    WICS-GUI-logs.png
+);
+
+for my $file (@files){
+    copy(path($docs, $file), path($out, $file));
+}
